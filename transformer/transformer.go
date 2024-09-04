010f3dc644ed4fd9c906a0676cc3ab2115a36b9d
Administrator <gitlab@163.com>
2024-09-04 17:25:44 +0800

    fix markdown.content exceed max length 4096

--- transformer/transformer.go
+++ transformer/transformer.go
@@ -21,62 +21,68 @@ func getAlertColor(severity string) string {
 	}
 }
 
-// TransformToMarkdown transform alertmanager notification to wechat markdow message
-func TransformToMarkdown(notification model.Notification, grafanaURL string, alertDomain string) (markdown *model.WeChatMarkdown, robotURL string, err error) {
+// 限制微信 markdown 消息的最大长度
+const maxMarkdownLength = 4096
+
+// 分片发送的最大条数
+const maxAlertsPerMessage = 5
+
+// TransformToMarkdown transform alertmanager notification to wechat markdown message
+func TransformToMarkdown(notification model.Notification, grafanaURL string, alertDomain string) (markdowns []*model.WeChatMarkdown, robotURL string, err error) {
 
 	status := notification.Status
-
 	annotations := notification.CommonAnnotations
 	robotURL = annotations["wechatRobot"]
 
 	var buffer bytes.Buffer
+	alertCount := 0
 
+	// 函数用于检查 buffer 长度，并在超长时分片
+	flushBuffer := func() {
+		if buffer.Len() > 0 {
+			markdown := &model.WeChatMarkdown{
+				MsgType: "markdown",
+				Markdown: &model.Markdown{
+					Content: buffer.String(),
+				},
+			}
+			markdowns = append(markdowns, markdown)
+			buffer.Reset() // 清空 buffer 以便下一次使用
+		}
+	}
+
+	// 开始构建消息
 	buffer.WriteString(fmt.Sprintf("### 当前状态:%s \n", status))
-	// buffer.WriteString(fmt.Sprintf("#### 告警项:\n"))
 
 	for _, alert := range notification.Alerts {
 		labels := alert.Labels
-
-		// 动态获取 var-NameSpace 和 var-Container
+		// 动态获取 namespace, container, alertDomain
 		namespace := labels["namespace"]
 		container := labels["container"]
-		// 动态获取告警详情的域名
-		alertDomain := labels["alertDomain"]
-		// 获取告警等级
 		severity := labels["severity"]
-		// 获取对应的颜色
 		alertColor := getAlertColor(severity)
 
-		fmt.Printf("namespace:%s, container: %s, alertDomain: %s\n", namespace, container, alertDomain)
-		buffer.WriteString(fmt.Sprintf("\n # 告警: <font color='%s'> %s </font>\n", alertColor, annotations["summary"]))
-		// datacenter 为 victoriametrics 获取 prometheus时区分环境的 label
-		//buffer.WriteString(fmt.Sprintf("\n>【环境】 %s\n", labels["datacenter"]))
+		// 增加告警内容到 buffer
+		buffer.WriteString(fmt.Sprintf("\n# 告警: <font color='%s'>%s</font>\n", alertColor, annotations["summary"]))
 		buffer.WriteString(fmt.Sprintf("\n>【级别】 %s\n", severity))
 		buffer.WriteString(fmt.Sprintf("\n>【类型】 %s\n", labels["alertname"]))
 		buffer.WriteString(fmt.Sprintf("\n>【主机】 %s\n", labels["instance"]))
 		buffer.WriteString(fmt.Sprintf("\n>【内容】 %s\n", annotations["description"]))
-		buffer.WriteString(fmt.Sprintf("\n>【当前状态】%s \n", status))
+		buffer.WriteString(fmt.Sprintf("\n>【当前状态】 %s \n", status))
 		buffer.WriteString(fmt.Sprintf("\n>【触发时间】 %s\n", alert.StartsAt.Format("2006-01-02 15:04:05")))
-		//buffer.WriteString(fmt.Sprintf("\n [跳转Grafana看板](https://grafana.vnnox.com/d/PwMJtdvnr/k8s-chu-neng-cnanduat?orgId=1&var-origin_prometheus=&var-Node=All&var-NameSpace=%s&var-Container=%s&var-Pod=All)", namespace, container))
-		buffer.WriteString(fmt.Sprintf("\n [跳转Grafana看板](https://%s?orgId=1&var-origin_prometheus=&var-Node=All&var-NameSpace=%s&var-Container=%s&var-Pod=All)", grafanaURL, namespace, container))
-		buffer.WriteString(fmt.Sprintf("\n [告警规则详情](http://%s/alerts?search=)", alertDomain))
-		//buffer.WriteString("\n [告警规则详情](http://%s/alerts?search=)", alertDomain)
-		//buffer.WriteString(fmt.Sprintf("\n>告警级别: %s\n", labels["severity"]))
-		//buffer.WriteString(fmt.Sprintf("\n>告警类型: %s\n", labels["alertname"]))
-		//buffer.WriteString(fmt.Sprintf("\n>故障主机: %s\n", labels["instance"]))
-		//
-		//annotations := alert.Annotations
-		//buffer.WriteString(fmt.Sprintf("\n>告警主题: %s\n", annotations["summary"]))
-		//buffer.WriteString(fmt.Sprintf("\n>告警详情: %s\n", annotations["description"]))
-		//buffer.WriteString(fmt.Sprintf("\n> 触发时间: %s\n", alert.StartsAt.Format("2006-01-02 15:04:05")))
+		buffer.WriteString(fmt.Sprintf("\n[跳转Grafana看板](https://%s?orgId=1&var-NameSpace=%s&var-Container=%s)", grafanaURL, namespace, container))
+		buffer.WriteString(fmt.Sprintf("\n[告警规则详情](http://%s/alerts?search=)", alertDomain))
+
+		// 检查当前长度是否超过最大长度
+		if buffer.Len() >= maxMarkdownLength || alertCount >= maxAlertsPerMessage {
+			flushBuffer()
+			alertCount = 0 // 重置告警计数
+		}
+		alertCount++
 	}
 
-	markdown = &model.WeChatMarkdown{
-		MsgType: "markdown",
-		Markdown: &model.Markdown{
-			Content: buffer.String(),
-		},
-	}
+	// 最后一次刷新
+	flushBuffer()
 
 	return
 }
