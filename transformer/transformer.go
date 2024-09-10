a8020489623675d8b1c0241f683109cb0e15db14
Administrator <gitlab@163.com>
2024-09-10 18:11:44 +0800

    fix 告警规则详情

--- transformer/transformer.go
+++ transformer/transformer.go
@@ -65,16 +65,6 @@ func TransformToMarkdown(notification model.Notification, grafanaURL string, ale
 		buffer.WriteString(fmt.Sprintf("\n [日志详情](https://aws-au-loki-grafana.vnnox.com/d/o6-BGgnnk/loki-kubernetes-logs?orgId=1&from=now-1h&to=now&var-query=&var-namespace=au&var-stream=All&var-container=vnnox-middle-oauth)"))
 		buffer.WriteString(fmt.Sprintf("\n"))
 		buffer.WriteString(fmt.Sprintf("---------------------------------------------------------------------------\n"))
-
-		//buffer.WriteString("\n [告警规则详情](http://%s/alerts?search=)", alertDomain)
-		//buffer.WriteString(fmt.Sprintf("\n>告警级别: %s\n", labels["severity"]))
-		//buffer.WriteString(fmt.Sprintf("\n>告警类型: %s\n", labels["alertname"]))
-		//buffer.WriteString(fmt.Sprintf("\n>故障主机: %s\n", labels["instance"]))
-		//
-		//annotations := alert.Annotations
-		//buffer.WriteString(fmt.Sprintf("\n>告警主题: %s\n", annotations["summary"]))
-		//buffer.WriteString(fmt.Sprintf("\n>告警详情: %s\n", annotations["description"]))
-		//buffer.WriteString(fmt.Sprintf("\n> 触发时间: %s\n", alert.StartsAt.Format("2006-01-02 15:04:05")))
 	}
 
 	markdown = &model.WeChatMarkdown{
