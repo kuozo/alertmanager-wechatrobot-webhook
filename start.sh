80ecf593729230d705082f7d41620a93a0f8d51b
Kollin <kollinchu@gmail.com>
2025-07-31 10:42:10 +0800

    Update start.sh
    fixed bash to sh,  and main function args error

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 function main() {
     /usr/bin/wechat-webhook -RobotKey $1 -addr $2 -grafanaUrl $3 -alertDomain $4 &
@@ -7,6 +7,6 @@ function main() {
     done
 }
 
-main "$1" "$2" "$3" "$4
+main "$1" "$2" "$3" "$4"
 
 nohup /usr/bin/wechat-webhook -RobotKey "77d13fe6-0047-48bc-803d-900892" -addr ":8888" -grafanaUrl "grafana.vnnox.com/d/PwMJtdvnr/k8s-chu-neng-cnanduat" -alertDomain "emscn-prometheus.ampaura.tech" &> emscn.log &
